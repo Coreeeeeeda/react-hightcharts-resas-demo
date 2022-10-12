1b895ad8f8657c163216c75c926f4f16874b4e1e
Mizuki Koreeda <this.branch.dev@gmail.com>
2022-10-13 08:44:49 +0900

    webpackのインストールと設定

---
+++ app/webpack.dev.js
@@ -0,0 +1,25 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    open: true,
+    host: "0.0.0.0",
+    port: 3000,
+    static: './public',
+  },
+});
